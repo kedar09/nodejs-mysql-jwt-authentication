18fe8c8e3bf5b4e26236ee7fcbca23e066c49de9
httpskedar <32976533+httpskedar@users.noreply.github.com>
2020-05-14 02:01:36 +0530

    Update auth.utility.js

--- app/AuthComponent/auth.utility.js
+++ app/AuthComponent/auth.utility.js
@@ -1,23 +1,23 @@
-var conn = require('../../config/database');
-var connection = conn.getConnection();
+const conn = require('../../config/database');
+let connection = conn.getConnection();
 //connect to database
 connection.connect();
 
 const jwt = require("jsonwebtoken");
- 
-exports.registerUser = function(req, result){
-    connection.query('INSERT INTO auth SET ?', [req.body], function (error, fields) {
-        if (error){
+
+exports.registerUser = function (req, result) {
+    connection.query('INSERT INTO auth SET ?', [req.body], function (error, result) {
+        if (error) {
             let resultRegisterUser = {message: 'User not registered'};
             result(null, resultRegisterUser);
         } else {
             let payload = {
                 email: req.body.email
-            }
-            let token = jwt.sign({ payload }, 'httpskedar', { expiresIn: '1d' });
-            let resultRegisterUser = {message: 'User register successfully', token: token};
+            };
+            let token = jwt.sign({payload}, 'httpskedar', {expiresIn: '1d'});
+            let resultRegisterUser = {message: 'User register successfully', token: token, authId: result.insertId};
             result(null, resultRegisterUser);
         }
     });
-}
+};
 
