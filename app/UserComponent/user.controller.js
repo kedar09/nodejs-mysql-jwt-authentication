15e346a51c45a5db275acfe075657934254ea92d
httpskedar <32976533+httpskedar@users.noreply.github.com>
2020-05-14 02:02:35 +0530

    Update user.controller.js

--- app/UserComponent/user.controller.js
+++ app/UserComponent/user.controller.js
@@ -1,9 +1,9 @@
-var userModel = require('./user.model');
+const userModel = require('./user.model');
 
 exports.getUserProfileById = function (req, res) {
     userModel.getUserProfileById(req, function (error, result) {
         if (error) {
-            res.send(error);
+            res.status(400).send(error);
         } else {
             res.status(200).send(result);
         }
@@ -11,31 +11,31 @@ exports.getUserProfileById = function (req, res) {
 };
 
 exports.updateUserPassword = function (req, res) {
-    userModel.updateUserPassword(req, function (err, task) {
-        if (err) {
-            res.send(err);
+    userModel.updateUserPassword(req, function (error, result) {
+        if (error) {
+            res.status(400).send(error);
         } else {
-            res.json(task);
+            res.status(200).send(result);
         }
     });
 };
 
 exports.updateUserProfile = function (req, res) {
-    userModel.updateUserProfile(req, function (err, task) {
-        if (err) {
-            res.send(err);
+    userModel.updateUserProfile(req, function (error, result) {
+        if (error) {
+            res.status(400).send(error);
         } else {
-            res.json(task);
+            res.status(200).send(result);
         }
     });
 };
 
 exports.deleteUserProfile = function (req, res) {
-    userModel.deleteUserProfile(req, function (err, task) {
-        if (err) {
-            res.send(err);
+    userModel.deleteUserProfile(req, function (error, result) {
+        if (error) {
+            res.status(400).send(error);
         } else {
-            res.json(task);
+            res.status(200).send(result);
         }
     });
 };
