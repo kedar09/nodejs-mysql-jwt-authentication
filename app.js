b9a3bfeafd1bc048a45f8842108052b13706bd7e
httpskedar <32976533+httpskedar@users.noreply.github.com>
2020-05-14 02:06:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var createError = require('http-errors');
 
-var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
 var usersRouter = require('./routes/users');
 
@@ -14,44 +13,39 @@ var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/auth',authRouter);
-app.use('/users',jwtVerify ,usersRouter);
+app.use('/auth', authRouter);
+app.use('/users', jwtVerify, usersRouter);
 
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
-function jwtVerify (req, res, next) {
-  console.log('verifying token...')
-  
-  if (req.headers.authorization) {
-      const token = req.headers.authorization.split(' ')[1]; // Bearer <token>
-      try {
-        //   console.log(token)
-        result = jwt.verify(token, 'httpskedar');
-        req.decoded = result;
-        // console.log(result);
-        next();
-      } catch (err) {
-      result = { 
-        error: `Unauthorized`,
-        status: 401
-      };
-           res.status(401).send(result);
-        // throw new Error(err);
-      }
+function jwtVerify(req, res, next) {
+    console.log('verifying token...');
+
+    if (req.headers.authorization) {
+        const token = req.headers.authorization.split(' ')[1]; // Bearer <token>
+        try {
+            result = jwt.verify(token, 'httpskedar');
+            req.decoded = result;
+            next();
+        } catch (err) {
+            result = {
+                error: `Unauthorized`,
+                status: 401
+            };
+            res.status(401).send(result);
+        }
     } else {
-      result = { 
-        error: `Unauthorized error. Token required.`,
-        status: 401
-      };
-      res.status(401).send(result);
+        result = {
+            error: `Unauthorized error. Token required.`,
+            status: 401
+        };
+        res.status(401).send(result);
     }
 }
 
