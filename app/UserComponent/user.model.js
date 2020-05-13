75c85a269030757ebd4e40166f647eeeef7fa560
httpskedar <32976533+httpskedar@users.noreply.github.com>
2020-05-14 02:02:59 +0530

    Update user.model.js

--- app/UserComponent/user.model.js
+++ app/UserComponent/user.model.js
@@ -1,76 +1,76 @@
-var conn = require('../../config/database');
-var connection = conn.getConnection();
+const conn = require('../../config/database');
+let connection = conn.getConnection();
 //connect to database
 connection.connect();
 
-var userUtility = require('./user.utility');
+const userUtility = require('./user.utility');
 
-// bcrypt - for password encryption 
+// bcrypt - for password encryption
 const bcrypt = require('bcrypt');
 
 exports.getUserProfileById = async function (req, result) {
-    connection.query("select count(*) as count from auth where authId=?", [req.body.authId],   
-        function(err,recordsArray,fields){
-            if (err){
+    connection.query("select count(*) as count from auth where authId=?", [req.body.authId],
+        function (err, records) {
+            if (err) {
                 console.log("Error occured while fetching the data !")
-            }else{
-                if(recordsArray[0].count==0){
-                    let resultDeleteUser = {message: 'User not found'};
+            } else {
+                if (records[0].count === 0) {
+                    let resultDeleteUser = {message: 'User not found!'};
                     result(null, resultDeleteUser);
                 } else {
                     userUtility.getUserProfileById(req, result);
                 }
             }
-    });
+        });
 };
 
 exports.updateUserPassword = function (req, result) {
-    connection.query("select count(*) as count from auth where authId=?", [req.body.authId],   
-        function(error, recordsArray, fields){
-            if (error){
+    connection.query("select count(*) as count from auth where authId=?", [req.body.authId],
+        function (error, records) {
+            if (error) {
                 console.log("Error occured while fetching the data !")
-            }else{
-                if(recordsArray[0].count==0){
-                    let resultUpdateUserPassword = {message: 'User not found'};
+            } else {
+                if (records[0].count === 0) {
+                    let resultUpdateUserPassword = {message: 'User not found!'};
                     result(null, resultUpdateUserPassword);
                 } else {
-                    req.body.password = bcrypt.hashSync(req.body.password,10);
+                    req.body.password = bcrypt.hashSync(req.body.password, 10);
                     userUtility.updateUserPassword(req, result);
                 }
             }
-    });
+        });
 };
 
 exports.updateUserProfile = function (req, result) {
-    connection.query("select count(*) as count from auth where authId=?", [req.body.authId],   
-        function(error, recordsArray, fields){
-            if (error){
+    connection.query("select count(*) as count from auth where authId=?", [req.body.authId],
+        function (error, records) {
+            if (error) {
                 console.log("Error occured while fetching the data !")
-            }else{
-                if(recordsArray[0].count==0){
-                    let resultUpdateUserProfile = {message: 'User not found'};
+            } else {
+                if (records[0].count === 0) {
+                    let resultUpdateUserProfile = {message: 'User not found!'};
                     result(null, resultUpdateUserProfile);
                 } else {
                     userUtility.updateUserProfile(req, result);
                 }
             }
-    });
+        });
 };
 
 exports.deleteUserProfile = function (req, result) {
     connection.query("select count(*) as count from auth where authId=?",
-        [req.body.authId],   
-        function(error, recordsArray, fields){
-        if (error){
-            console.log("Error occured while fetching the data !")
-        }else{
-            if(recordsArray[0].count==0){
-                 let resultDeleteUser = {message: 'User not found'};
-                 result(null, resultDeleteUser);
+        [req.body.authId],
+        function (error, records) {
+            if (error) {
+                console.log("Error occured while fetching the data !")
             } else {
-                userUtility.deleteUserProfile(req, result);
+                if (records[0].count === 0) {
+                    let resultDeleteUser = {message: 'User not found!'};
+                    result(null, resultDeleteUser);
+                } else {
+                    userUtility.deleteUserProfile(req, result);
+                }
             }
-        }
-    });
+        });
 };
 
