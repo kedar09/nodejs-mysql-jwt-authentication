6cfd531f130e19f1085698d3b13204f22a767277
kedar09 <32976533+kedar09@users.noreply.github.com>
2021-05-24 20:23:52 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "root",
-  database: "users_database",
+  database: "TEST",
 });
 
 connection.connect();
